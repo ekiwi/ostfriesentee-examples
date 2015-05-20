37239bbfdd513a58ea49faf0641a1382d4c275f1
Electronic Kiwi <electron.kiwi@gmail.com>
2015-05-20 17:54:48 +0200

    [check.py] add script to build examples

---
+++ check.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
+#
+# This file is part of Ostfriesentee.
+#
+# Ostfriesentee is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ostfriesentee is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Ostfriesentee.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+sys.path.append('ostfriesentee/scripts')
+from checker import Checker
+
+if __name__ == "__main__":
+	c = Checker()
+	c.run(['app'])
+	sys.exit(c.printreport())
