f816d7a9e93085498be0d6e9433717c6d53f9775
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-22 21:16:22 +0200

    [posix] SimpleObject.hpp: use new setParam methods

--- posix/src/SimpleObject.hpp
+++ posix/src/SimpleObject.hpp
@@ -1,14 +1,12 @@
 #ifndef SIMPLEOBJECT_HPP
 #define SIMPLEOBJECT_HPP
 
-#include <hpp/object.hpp>
-#include <hpp/infusion.hpp>
-#include <hpp/vm.hpp>
-using namespace ostfriesentee;
+#include <hpp/ostfriesentee.hpp>
+
 
 namespace jlib_object {
 
-class SimpleObject : Object {
+class SimpleObject : ostfriesentee::Object {
 	static constexpr uint8_t ClassId = 0;
 
 	_OBJECT_STRUCT_SimpleObject* obj;
@@ -19,18 +17,17 @@ public:
 	SimpleObject& operator=(const SimpleObject&) = delete;
 
 public:
-	SimpleObject(Infusion& infusion, int32_t a, int32_t b) {
+	SimpleObject(ostfriesentee::Infusion& infusion, int32_t a, int32_t b) {
 		this->infusion = infusion.getUnderlying();
 		this->obj = (_OBJECT_STRUCT_SimpleObject*)create(this->infusion, ClassId);
 		dj_mem_addSafePointer((void**)&this->obj);
 
 		// parameters
 		int16_t intParams[4];
-		intParams[0] = (a >>  0) & 0xffff;
-		intParams[1] = (a >> 16) & 0xffff;
-		intParams[2] = (b >>  0) & 0xffff;
-		intParams[3] = (b >> 16) & 0xffff;
-		ref_t refParams[1] = { VOIDP_TO_REF(this->obj) };
+		ref_t refParams[1];
+		setParam(intParams, 0, a);
+		setParam(intParams, 2, b);
+		setParam(refParams, 0, this->obj);
 
 		dj_global_id ctor{this->infusion, OBJECT_MDEF_void__init__int_int};
 		dj_exec_callMethodFromNative(ctor, getThread(), refParams, intParams);
@@ -46,9 +43,10 @@ public:
 	}
 
 	int32_t getA() {
+		int16_t intParams[2] = { 0, 0 };
 		ref_t refParams[1] = { VOIDP_TO_REF(this->obj) };
 		dj_global_id method{this->infusion, OBJECT_MDEF_int_getA};
-		dj_exec_callMethodFromNative(method, getThread(), refParams, nullptr);
+		dj_exec_callMethodFromNative(method, getThread(), refParams, intParams);
 		dj_exec_run(100000);
 		return 0;
 	}
