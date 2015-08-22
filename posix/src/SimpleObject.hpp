1e163a7e69af4cb9d9dfa9a8b573b6c07adc636a
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2015-08-22 21:46:11 +0200

    [posix] SimpleObject.hpp: use new runMethod method

--- posix/src/SimpleObject.hpp
+++ posix/src/SimpleObject.hpp
@@ -10,15 +10,13 @@ class SimpleObject : ostfriesentee::Object {
 	static constexpr uint8_t ClassId = 0;
 
 	_OBJECT_STRUCT_SimpleObject* obj;
-	dj_infusion* infusion;
 
 public:
 	SimpleObject(const SimpleObject&) = delete;
 	SimpleObject& operator=(const SimpleObject&) = delete;
 
 public:
-	SimpleObject(ostfriesentee::Infusion& infusion, int32_t a, int32_t b) {
-		this->infusion = infusion.getUnderlying();
+	SimpleObject(ostfriesentee::Infusion& infusion, int32_t a, int32_t b) : ostfriesentee::Object(infusion) {
 		this->obj = (_OBJECT_STRUCT_SimpleObject*)create(this->infusion, ClassId);
 		dj_mem_addSafePointer((void**)&this->obj);
 
@@ -29,9 +27,7 @@ public:
 		setParam(intParams, 2, b);
 		setParam(refParams, 0, this->obj);
 
-		dj_global_id ctor{this->infusion, OBJECT_MDEF_void__init__int_int};
-		dj_exec_callMethodFromNative(ctor, getThread(), refParams, intParams);
-		dj_exec_run(100000);
+		this->runMethod(OBJECT_MDEF_void__init__int_int, refParams, intParams);
 	}
 
 	~SimpleObject() {
@@ -45,9 +41,7 @@ public:
 	int32_t getA() {
 		int16_t intParams[2] = { 0, 0 };
 		ref_t refParams[1] = { VOIDP_TO_REF(this->obj) };
-		dj_global_id method{this->infusion, OBJECT_MDEF_int_getA};
-		dj_exec_callMethodFromNative(method, getThread(), refParams, intParams);
-		dj_exec_run(100000);
+		this->runMethod(OBJECT_MDEF_int_getA, refParams, intParams);
 		return 0;
 	}
 
